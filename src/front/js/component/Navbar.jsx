ced2a37819fe8b5bf3f9d60817c4f125f3e22401
Mathew Woo <133248984+MatWoov@users.noreply.github.com>
2023-11-08 22:41:33 +0000

    wip: added code to Navbar component

--- src/front/js/component/Navbar.jsx
+++ src/front/js/component/Navbar.jsx
@@ -1,20 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import logoWoofPetShop from "../../img/logo-pet-shop.jpg"
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <div className="row">
+      <nav className="navbar navbar-expand-lg bg-primary" data-bs-theme="white">
+        <div className="col ms-5">
+          <button type="button" className="btn btn-primary">
+            <img src={logoWoofPetShop} style={{width: "50px", height: "50px"}} alt="" />
+          </button>
+        </div>
+        <div className="col-4">
+          <form className="d-flex">
+            <input className="form-control me-sm-2" type="search" placeholder="Search"/>
+            <button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+        <div className="col text-end me-5">
+        <button type="button" className="btn btn-primary"><i className="fas fa-lock me-2"></i>Login</button>
+        <span className="border-start"></span>
+        <button type="button" className="btn btn-primary me-1"><i className="fas fa-heart me-2"></i>Favorites</button>
+        <button type="button" className="btn btn-primary"><i className="fas fa-shopping-cart"></i></button>
+        </div>
+      </nav>
+    </div>
+  );
 };
