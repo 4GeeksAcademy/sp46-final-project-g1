d645a43460458e0db793004890d482987fe9166d
Mathew Woo <133248984+MatWoov@users.noreply.github.com>
2023-11-11 12:15:22 +0000

    wip: fixed code and added comment

--- src/front/js/component/StarReating.jsx
+++ src/front/js/component/StarReating.jsx
@@ -3,7 +3,6 @@ import { useState } from "react";
 import { FaStar } from "react-icons/fa";
 
 
-
 export const StarReating = () => {
     const stars = Array(5).fill(0);
     const [currentValue, setCurrentValue] = useState(0)
@@ -14,18 +13,12 @@ export const StarReating = () => {
         grey: "#a9a9a9"
     }
     
-
+    const handleMouseOver = (value) => {setHoverValue(value)};
+    const handleMouseLeave = () => {setHoverValue(undefined)};
     const handleClick= (value) => {
-        setCurrentValue(value)
-    };
+        setCurrentValue(value)  //Aqui deberiamos guardar la valoracion en los modelos (back).
+    };  
 
-    const handleMouseOver = (value) => {
-        setHoverValue(value)
-    };
-
-    const handleMouseLeave = () => {
-        setHoverValue(undefined)
-    };
 
     return (
         <div className="container d-flex justify-content-center">
@@ -33,15 +26,13 @@ export const StarReating = () => {
                 {stars.map((_, index) => {
                     return (
                         <FaStar 
-                        key={index}
-                        size={17}
-                        style={{
-                            cursor: "pointer"
-                        }}
-                        color={(hoverValue || currentValue) > index ? colors.orange : colors.grey}
-                        onClick={() => handleClick(index + 1)}
-                        onMouseOver={() => handleMouseOver(index + 1)}
-                        onMouseLeave={handleMouseLeave}
+                            key={index}
+                            size={17}
+                            style={{cursor: "pointer"}}
+                            color={(hoverValue || currentValue) > index ? colors.orange : colors.grey}
+                            onClick={() => handleClick}
+                            onMouseOver={() => handleMouseOver(index + 1)}
+                            onMouseLeave={handleMouseLeave}
                         />
                     )
                 })}
