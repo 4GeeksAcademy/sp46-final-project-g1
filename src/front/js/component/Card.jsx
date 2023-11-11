e429bb11ab0509930157cd55c37ee6de0e936649
Mathew Woo <133248984+MatWoov@users.noreply.github.com>
2023-11-11 11:07:05 +0000

    wip: changed class for className

--- src/front/js/component/Card.jsx
+++ src/front/js/component/Card.jsx
@@ -6,29 +6,29 @@ export const Card = () => {
 
     return (
         <section style={{ background: '#eee' }}>
-            <div class="container py-5">
-                <div class="card" style={{ width: '250px', height: '375px' }}>
-                    <div class="d-flex justify-content-end p-3">
-                        <button type="button" class="btn btn-outline-primary btn-sm rounded-circle d-flex align-items-center justify-content-center shadow-1-strong"
+            <div className="container py-5">
+                <div className="card" style={{ width: '250px', height: '375px' }}>
+                    <div className="d-flex justify-content-end p-3">
+                        <button type="button" className="btn btn-outline-primary btn-sm rounded-circle d-flex align-items-center justify-content-center shadow-1-strong"
                             style={{ width: '35px', height: '35px' }}>
-                            <i class="far fa-heart"></i>
+                            <i className="far fa-heart"></i>
                         </button>
                     </div>
                     <img src="https://img.freepik.com/vector-gratis/ilustracion-concepto-computo-escritorio-moderno_114360-12156.jpg"
-                        class="card-img-top" alt="Laptop" />
-                    <div class="card-body">
-                        <div class="d-flex justify-content-between">
-                            <p class="small"><a href="#!" class="text-muted">Laptops</a></p>
+                        className="card-img-top" alt="Laptop" />
+                    <div className="card-body">
+                        <div className="d-flex justify-content-between">
+                            <p className="small"><a href="#!" className="text-muted">Laptops</a></p>
                         </div>
 
-                        <div class="d-flex justify-content-between mb-3">
-                            <h5 class="mb-0">HP Notebook</h5>
-                            <h5 class="text-dark mb-0">500€</h5>
+                        <div className="d-flex justify-content-between mb-3">
+                            <h5 className="mb-0">HP Notebook</h5>
+                            <h5 className="text-dark mb-0">500€</h5>
                         </div>
 
-                        <div class="d-flex justify-content-between mb-2">
-                            <p class="text-muted mt-1">Available: <span class="fw-bold">6</span></p>
-                            <div class="ms-auto text-warning">
+                        <div className="d-flex justify-content-between mb-2">
+                            <p className="text-muted mt-1">Available: <span className="fw-bold">6</span></p>
+                            <div className="ms-auto text-warning">
                                 <StarReating />
                             </div>
                         </div>
