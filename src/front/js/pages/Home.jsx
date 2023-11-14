ba48d1dc5aec11c3b9d80d43083e35b4400a2adc
Mathew Woo <133248984+MatWoov@users.noreply.github.com>
2023-11-14 21:57:14 +0000

    wip: added jumbotron in Home page

--- src/front/js/pages/Home.jsx
+++ src/front/js/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 
 export const Home = () => {
@@ -15,24 +16,24 @@ export const Home = () => {
 					<img src="https://kb.rspca.org.au/wp-content/uploads/2021/07/collie-beach-bokeh.jpg"
 						className="d-block w-100" style={{ height: "450px", objectFit: "cover" }} alt="road trip dog" />
 					<div className="carousel-caption d-flex align-items-start flex-column">
-						<h4 className="text-dark"><strong>This line rendered as bold text</strong></h4>
-						<p className="text-dark">Some representative placeholder content for the first slide.</p>
+						<h4 className="text-dark"><strong>Welcome! Here you will find<br />the best products for your best friend</strong></h4>
+						<Link to="/" className="btn btn-danger"><i className="fas fa-paw me-2"></i>All products</Link>
 					</div>
 				</div>
 				<div className="carousel-item">
 					<img src="https://assets-au-01.kc-usercontent.com/ab37095e-a9cb-025f-8a0d-c6d89400e446/fba30a1f-3490-4761-9f92-f28bf27e46ff/article-interpreting-body-language-in-dogs.jpg"
 						className="d-block w-100" style={{ height: "450px", objectFit: "cover" }} alt="dog window" />
-					<div className="carousel-caption d-none d-md-block">
-						<h5>First slide label</h5>
-						<p>Some representative placeholder content for the first slide.</p>
+					<div className="carousel-caption d-flex align-items-start flex-column">
+						<h4 className="text-light"><strong>Woof! Do you have an account?</strong></h4>
+						<Link to="/" className="btn btn-danger"><i className="fas fa-user me-2"></i>Sign in</Link>
 					</div>
 				</div>
 				<div className="carousel-item">
 					<img src="https://naturesmiracle.la/cl/wp-content/uploads/sites/2/2020/12/iStock-1013878116-1-scaled.jpg"
 						className="d-block w-100" style={{ height: "450px", objectFit: "cover" }} alt="dog park" />
-					<div className="carousel-caption d-none d-md-block">
-						<h5>First slide label</h5>
-						<p>Some representative placeholder content for the first slide.</p>
+					<div className="carousel-caption d-flex align-items-start flex-column">
+						<h4 className="text-light"><strong>I'm smelling something tasty! <br />Check out these offers</strong></h4>
+						<Link to="/" className="btn btn-danger"><i className="fas fa-bone fa-spin me-2"></i>Offers</Link>
 					</div>
 				</div>
 			</div>
